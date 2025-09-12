b7a8c69681a69eaffc710fd73347cd850eafbd37
Ravi Ojha <ivarojha@gmail.com>
2025-09-13 03:12:18 +0530

    Use docs.ticketping.com for nextjs assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 import { createMDX } from 'fumadocs-mdx/next';
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 
 const withMDX = createMDX();
+const isDev = phase === PHASE_DEVELOPMENT_SERVER
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  assetPrefix: isDev ? undefined : 'https://docs.ticketping.com',
 };
 
 export default withMDX(config);
