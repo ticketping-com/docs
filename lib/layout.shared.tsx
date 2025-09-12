0bda13bc58e1e3116d637a96b21a0a292e79a0d2
Ravi Ojha <ivarojha@gmail.com>
2025-09-13 03:24:32 +0530

    Use /docs/ as asset prefix

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -12,7 +12,7 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#a)"><path fill="#020202" d="M32 16a16 16 0 0 1 -16 16A16 16 0 0 1 0 16a16 16 0 0 1 32 0"/><path d="M13.982 11.406h6.601l-0.863 4.062H13.982z" fill="#fff"/><path d="M13.761 8.005h4.253l-3.746 16.037h-4.277z" fill="#fff"/><path fill="#4CB782" fill-opacity=".9" d="M24 20.954a3.313 3.313 0 0 1 -3.313 3.313 3.313 3.313 0 0 1 -3.313 -3.313 3.313 3.313 0 0 1 6.626 0"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h32v32H0z"/></clipPath></defs></svg>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#a)"><path fill="#020202" d="M32 16a16 16 0 0 1 -16 16A16 16 0 0 1 0 16a16 16 0 0 1 32 0"/><path d="M13.982 11.406h6.601l-0.863 4.062H13.982z" fill="#fff"/><path d="M13.761 8.005h4.253l-3.746 16.037h-4.277z" fill="#fff"/><path fill="#4CB782" fillOpacity=".9" d="M24 20.954a3.313 3.313 0 0 1 -3.313 3.313 3.313 3.313 0 0 1 -3.313 -3.313 3.313 3.313 0 0 1 6.626 0"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h32v32H0z"/></clipPath></defs></svg>
           Ticketping Docs
         </>
       ),
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 import { createMDX } from 'fumadocs-mdx/next';
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
 
 const withMDX = createMDX();
-const isDev = phase === PHASE_DEVELOPMENT_SERVER
 
 /** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true,
-  assetPrefix: isDev ? undefined : 'https://docs.ticketping.com',
+const config = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  
+  return {
+    reactStrictMode: true,
+    assetPrefix: isDev ? '/docs/' : 'https://docs.ticketping.com/docs/',
+  };
 };
 
 export default withMDX(config);
