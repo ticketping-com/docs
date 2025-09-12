57036c42ab9227e868f70019eacf5c59fe1e7eca
Ravi Ojha <ivarojha@gmail.com>
2025-09-13 02:06:34 +0530

    Add basic docs

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -12,15 +12,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#a)"><path fill="#020202" d="M32 16a16 16 0 0 1 -16 16A16 16 0 0 1 0 16a16 16 0 0 1 32 0"/><path d="M13.982 11.406h6.601l-0.863 4.062H13.982z" fill="#fff"/><path d="M13.761 8.005h4.253l-3.746 16.037h-4.277z" fill="#fff"/><path fill="#4CB782" fill-opacity=".9" d="M24 20.954a3.313 3.313 0 0 1 -3.313 3.313 3.313 3.313 0 0 1 -3.313 -3.313 3.313 3.313 0 0 1 6.626 0"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h32v32H0z"/></clipPath></defs></svg>
+          Ticketping Docs
         </>
       ),
     },
